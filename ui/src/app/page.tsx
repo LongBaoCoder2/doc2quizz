d512669b51f25cb026a432ae7569d618d70453ab
LongBaoCoder2 <longbao.it@gmail.com>
2024-07-28 22:28:07 +0700

    feat: ui for app

---
+++ ui/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-6xl font-bold">Hello World👋</h1>
+    </main>
+  )
+}
